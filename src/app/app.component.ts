83a5ea9c7b48ac71e9fa758f1fc9490fd5be77b8
mahesh.kumar4 <mahesh.kumar4@delhivery.com>
2019-02-01 19:43:12 +0530

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-	title = 'game-boxes';
 
+	number: any;
+
+
+	title = 'game-boxes';
 	rows: number;
 	noOfColorBoxes: number;
 	dificulty: number;
 
-	colorList: Array<string> = ['#0000ff', '#9c27b0', '#e91e63', '#607d8b', '#009688', '#8bc34a', '#ffeb3b', '#ffc107']
-	colorBoxes: Array<number>;
+	colorList: Array<string> = ['#0000ff', '#3230d2', '#de5bd4', '#845180', '#5c0c8e', '#9c27b0', '#e91e63', '#607d8b', '#009688', '#8bc34a', '#ffeb3b', '#ffc107', '#1a0d4a', '#3ec9d0', '#b3d03e']
+	colorBoxes: Array<string>;
 	uncoloredBoxes: Array<number>;
 	boxes: any;
 	poll;
@@ -30,6 +33,11 @@ export class AppComponent implements OnInit {
 
 	}
 
+	ngOnInit() {
+
+		this.buildForm();
+	}
+
 	buildForm() {
 		this.form = this.formBuilder.group({
 			noOfBoxes: ['', [Validators.required, Validators.min(0), Validators.max(2000), Validators.pattern("^[0-9]*$")]],
@@ -40,39 +48,70 @@ export class AppComponent implements OnInit {
 
 	onSubmit() {
 
-		console.log(this.form.controls);
 		let formValues = this.form.controls;
 
-		// stop here if form is invalid
 		if (this.form.invalid) {
 			return;
 		}
 
-		this.rows = formValues.noOfBoxes.value;
-		this.noOfColorBoxes = formValues.noOfColorBoxes.value;
-		this.dificulty = formValues.dificultyLevel.value * 1000;
+		this.rows = +formValues.noOfBoxes.value;
+		this.noOfColorBoxes = +formValues.noOfColorBoxes.value;
+		this.dificulty = +formValues.dificultyLevel.value * 1000;
 		this.totalBoxes = this.rows * this.rows;
+
+		console.log(this.noOfColorBoxes);
+
+
+		//this.number = Array(this.rows).fill(0)
 
 		if (this.noOfColorBoxes >= this.totalBoxes) {
 			this.gameWon();
 			return;
 		}
-
 		this.isFormSubmitted = true;
 
-		this.setDefaultColorBoxes();
+		let i = 0;
+		var table = document.createElement("table");
+		while (i < this.rows) {
+			let j = 0;
+			let tr = document.createElement("tr");
+			while (j < this.rows) {
+				let td = document.createElement("td");
+				td.className = "td-bg";
+				td.id = 'A' + ((i * this.rows + 1) + j);
+				tr.appendChild(td);
+				j++;
+			}
+			table.appendChild(tr);
+			i++;
+		}
 
-		this.setDefaultUncoloredBoxes();
 
-		this.initilizeGame();
 
-		this.polling();
+		//console.log(this.number);
 
-	}
 
-	ngOnInit() {
+		console.time('timeStart');
 
-		this.buildForm();
+
+		setTimeout(() => {
+			let box = document.getElementById('box');
+			box.appendChild(table);
+			
+			this.setDefaultColorBoxes();
+
+			this.polling();
+		})
+
+
+		/*		
+	
+			this.setDefaultUncoloredBoxes();
+	
+			this.initilizeGame();
+	
+			*/
+
 	}
 
 
@@ -80,80 +119,90 @@ export class AppComponent implements OnInit {
 		let arr = [];
 		while (arr.length < this.noOfColorBoxes) {
 			let r = Math.floor(Math.random() * this.totalBoxes) + 1;
-			if (arr.indexOf(r) === -1) arr.push(r);
+			if (arr.indexOf(r) === -1) arr.push('A' + r);
 		}
 		this.colorBoxes = arr;
-	}
+
+		console.log(this.colorBoxes);
 
 
-	setDefaultUncoloredBoxes() {
-		let arr = [];
-		for (let i = 1; i <= this.totalBoxes; i++) {
-			if (this.colorBoxes.indexOf(i) === -1) {
-				arr.push(i);
-			}
+		for (let i of this.colorBoxes) {
+			console.log(i);
+			document.getElementById(i).style.backgroundColor = this.getRandommColor();
 		}
-		this.uncoloredBoxes = arr;
-	}
-
-	checkInColorBoxes(boxId) {
-		if (this.colorBoxes.indexOf(boxId) > -1) {
-			return this.getRandommColor();
-		}
-		return '';
 	}
 
 
-	initilizeGame() {
-		let items = [];
+	// setDefaultUncoloredBoxes() {
+	// 	let arr = [];
+	// 	for (let i = 1; i <= this.totalBoxes; i++) {
+	// 		if (this.colorBoxes.indexOf(i) === -1) {
+	// 			arr.push(i);
+	// 		}
+	// 	}
+	// 	this.uncoloredBoxes = arr;
+	// }
 
-		for (let i = 0; i < this.rows; i++) {
-			let innerArr = [];
+	// checkInColorBoxes(boxId) {
+	// 	if (this.colorBoxes.indexOf(boxId) > -1) {
+	// 		return this.getRandommColor();
+	// 	}
+	// 	return '';
+	// }
 
-			for (let j = 0; j < this.rows; j++) {
-				let boxId = ((i * this.rows) + 1) + j;
-				innerArr.push({
-					id: boxId,
-					color: this.checkInColorBoxes(boxId)
-				})
-			}
-			items.push(innerArr);
-		}
-		this.boxes = items;
 
-	}
+	// initilizeGame() {
+	// 	let items = [];
 
-	changeColor(boxId) {
-		let box_id = parseInt(boxId)
+	// 	for (let i = 0; i < this.rows; i++) {
+	// 		let innerArr = [];
 
-		if (this.uncoloredBoxes.indexOf(box_id) === -1) return;
+	// 		for (let j = 0; j < this.rows; j++) {
+	// 			let boxId = ((i * this.rows) + 1) + j;
+	// 			innerArr.push({
+	// 				id: boxId,
+	// 				color: this.checkInColorBoxes(boxId)
+	// 			})
+	// 		}
+	// 		items.push(innerArr);
+	// 	}
+	// 	this.boxes = items;
+	// 	console.timeEnd('timeStart');
+	// }
 
-		this.uncoloredBoxes.splice(this.uncoloredBoxes.indexOf(box_id), 1);
-		this.colorBoxes.push(box_id);
+	changeColor(evt) {
+		
+		let box = evt.target;
+		console.log(box);
+		
+		if (box.style.backgroundColor && box.style.backgroundColor !== '#FFFFFF') return;
 
-		this.updateBox(box_id, 1)
+		//this.uncoloredBoxes.splice(this.uncoloredBoxes.indexOf(box_id), 1);
+		box.style.backgroundColor = this.getRandommColor();
+		this.colorBoxes.push(box.id);
 
-		if (!this.uncoloredBoxes.length) {
+		//this.updateBox(box_id, 1)
+
+		if (this.colorBoxes.length === this.totalBoxes) {
 			setTimeout(() => {
 				this.gameWon();
 				this.poll.unsubscribe();
-
 			})
 		}
 	}
 
-	updateBox(box_id, type) {
+	// updateBox(box_id, type) {
 
-		let ind_1 = this.firstIndex(box_id);
-		let ind_2 = ((box_id % this.rows) ? (box_id % this.rows) : this.rows) - 1;
+	// 	let ind_1 = this.firstIndex(box_id);
+	// 	let ind_2 = ((box_id % this.rows) ? (box_id % this.rows) : this.rows) - 1;
 
-		if (type === 1) {
-			this.boxes[ind_1][ind_2].color = this.getRandommColor();
-		} else if (type === 2) {
-			this.boxes[ind_1][ind_2].color = '';
-		}
+	// 	if (type === 1) {
+	// 		this.boxes[ind_1][ind_2].color = this.getRandommColor();
+	// 	} else if (type === 2) {
+	// 		this.boxes[ind_1][ind_2].color = '';
+	// 	}
 
-	}
+	// }
 
 	polling() {
 		this.poll = interval(this.dificulty).subscribe(
@@ -164,10 +213,10 @@ export class AppComponent implements OnInit {
 					let colorArr = this.colorBoxes;
 					let randomNo = this.getRandomNoFromArray(colorArr);
 
-					this.uncoloredBoxes.push(randomNo);
+					//this.uncoloredBoxes.push(randomNo);
 					this.colorBoxes.splice(this.colorBoxes.indexOf(randomNo), 1);
-
-					this.updateBox(randomNo, 2);
+					document.getElementById(randomNo).style.backgroundColor = '#FFFFFF';
+					//this.updateBox(randomNo, 2);
 
 					if (!this.colorBoxes.length) {
 						this.poll.unsubscribe();
@@ -180,12 +229,10 @@ export class AppComponent implements OnInit {
 			},
 			(error: any) => {
 				console.log('error');
-			},
-			() => {
-				console.log('observable completed !');
 			}
 		);
 	}
+
 
 	firstIndex(box_id) {
 		let d = box_id / this.rows;
@@ -204,7 +251,7 @@ export class AppComponent implements OnInit {
 	getRandomNoFromArray(items) {
 
 		let num = Math.floor(Math.random() * items.length);
-		return parseInt(items[num]);
+		return items[num];
 	}
 
 	gameWon() {
